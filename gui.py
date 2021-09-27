042a047625abdb153da8a1075ff8b64d729928ee
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-09-27 23:33:49 +0300

    background fix

--- gui.py
+++ gui.py
@@ -11,9 +11,17 @@ blank_space = " "
 root.title(90*blank_space+"CPNDC")
 root.geometry("700x400")
 
-#backgroud
-backgroud = ImageTk.PhotoImage(Image.open("./icons/backgroud.jpg"))  
-l= tk.Label(image=backgroud)
-l.pack()
+#background
+filename = ImageTk.PhotoImage(Image.open("./icons/backgroud.jpg"))
+background_label = tk.Label(root, image=filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+#search bar
+city_text = tk.StringVar()
+city_entry = tk.Entry(root, textvariable=city_text)
+city_entry.pack()
+
+Search = tk.Button(root, text="Search Weather")
+Search.pack()
 
 root.mainloop()
