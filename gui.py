ab38138cd06d038728499433c183236fe7a7848e
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-10-02 17:41:22 +0300

    price rem

--- gui.py
+++ gui.py
@@ -27,7 +27,10 @@ def generate_data():
     data = kot.generate_data(date,coin_text.get().lower())
     print(data)
 
-
+def notify():
+    price = price_text.get()
+    print(price)
+    
 #title
 root.title("CPNDC")
 root.geometry("1000x600")
@@ -59,10 +62,21 @@ price.grid(row=8,column=4)
 l2 = tk.Label(root,text = "Select date for price and other history",font=("bold", 14))
 l2.grid(row=100,column=0)
 mycal1 = Calendar(root,setmode="day", date_pattern = 'd/m/yy')
-mycal1.place(x=220, y=170)
+mycal1.place(x=220, y=200)
 Generate_Data = tk.Button(root, text="Generate Data",command=generate_data)
 Generate_Data.grid(row=4000, column=0)
 
+
+#price you want to get notified
+l3 = tk.Label(root,text = "Price to get notified",font=("bold", 14))
+l3.grid(row=0,column=70)
+
+price_text = tk.StringVar()
+price_entry = tk.Entry(root, textvariable=price_text)
+price_entry.grid(row=1, column=70, padx=0, pady=0)
+
+Set_Notifier = tk.Button(root, text="Set Notifier",command=notify)
+Set_Notifier.grid(row=2, column=70)
 #update the price every 5 seconds
 def rep():
     search_coin_clicked()
