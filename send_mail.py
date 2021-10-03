1267410edb1302bc3027f9604ed46e7f2efff3e7
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-10-03 22:12:07 +0300

    mail fin

--- send_mail.py
+++ send_mail.py
@@ -12,14 +12,12 @@ class Send_Mail():
         sender_email = self.email
         receiver_email = self.main_email
         password = self.password
-        # password = input("Type your password and press enter:")
 
         message = MIMEMultipart("alternative")
         message["Subject"] = "BUY THE DIP"
         message["From"] = sender_email
         message["To"] = receiver_email
 
-        # Create the plain-text and HTML version of your message
         text = """\
         Hi,
         How are you? Good? Wanna make money??
@@ -37,16 +35,11 @@ class Send_Mail():
         </html>
         """
 
-        # Turn these into plain/html MIMEText objects
         part1 = MIMEText(text, "plain")
         part2 = MIMEText(html, "html")
 
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
         message.attach(part1)
         message.attach(part2)
-
-        # Create secure connection with server and send email
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(sender_email, password)
