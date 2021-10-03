923c3a93b9c1e92982b936762941f8051aa1a92a
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-10-03 17:20:30 +0300

    des

--- gui.py
+++ gui.py
@@ -40,48 +40,54 @@ root.iconphoto(False, tk.PhotoImage(file='icons\icon.png'))
 
 #search bar for coin
 l1 = tk.Label(root,text = "Search Coin",font=("bold", 18))
-l1.grid(row=0,column=0)
+l1.grid(row=0,column=0, padx=0, pady=0)
 
 coin_text = tk.StringVar()
 coin_entry = tk.Entry(root, textvariable=coin_text)
-coin_entry.grid(row=2, column=0, padx=3, pady=3)
+coin_entry.grid(row=1, column=0, padx=0, pady=0)
 
 #button search coin    
 Search = tk.Button(root, text="Search Coin",command=search_coin_clicked)
-Search.grid(row=3, column=0, padx = 0, pady = 0)
+Search.grid(row=2, column=0, padx = 0, pady = 0)
 
 #text for coin name
 Coin_Name = tk.Label(root,text = "",font=("bold", 18))
-Coin_Name.grid(row=7,column=4)
+Coin_Name.grid(row=0,column=90,padx = 100)
 
 #text for price
 price = tk.Label(root,text = "",font=("bold", 12))
-price.grid(row=8,column=4)
+price.grid(row=1,column=90,padx = 100)
 
 #date selection for price history
-l2 = tk.Label(root,text = "Select date for price and other history",font=("bold", 14))
-l2.grid(row=100,column=0)
-mycal1 = Calendar(root,setmode="day", date_pattern = 'd/m/yy')
-mycal1.place(x=220, y=200)
-Generate_Data = tk.Button(root, text="Generate Data",command=generate_data)
-Generate_Data.grid(row=4000, column=0)
+# l2 = tk.Label(root,text = "Select date for price and other history",font=("bold", 14))
+# l2.grid(row=100,column=0)
+# mycal1 = Calendar(root,setmode="day", date_pattern = 'd/m/yy')
+# mycal1.place(x=220, y=200)
+# Generate_Data = tk.Button(root, text="Generate Data",command=generate_data)
+# Generate_Data.grid(row=4000, column=0)
 
 
-#price you want to get notified
-l3 = tk.Label(root,text = "Price to get notified",font=("bold", 14))
-l3.grid(row=0,column=70)
+# #price you want to get notified
+# l3 = tk.Label(root,text = "Price to get notified",font=("bold", 14))
+# l3.grid(row=0,column=70)
+
+l2 = tk.Label(root,text = "Enter Price",font=("bold", 18))
+l2.grid(row=60,column=0)
 
 price_text = tk.StringVar()
 price_entry = tk.Entry(root, textvariable=price_text)
-price_entry.grid(row=1, column=70, padx=0, pady=0)
+price_entry.grid(row=70, column=0, padx=0, pady=0)
 
 Set_Notifier = tk.Button(root, text="Set Notifier",command=notify)
-Set_Notifier.grid(row=2, column=70)
+Set_Notifier.grid(row=80, column=0)
 #update the price every 5 seconds
 def rep():
     search_coin_clicked()
     print(price["text"])
     root.after(5000, rep)
 root.after(5000, rep)
+
+
+#new window for adding sender, password of sender and your main email
 
 root.mainloop()
