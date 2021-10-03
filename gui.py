972e72d579af109a3abb87b119183145da655418
Geri Dashja <57563604+geridashja@users.noreply.github.com>
2021-10-03 17:41:03 +0300

    perf fix

--- gui.py
+++ gui.py
@@ -52,34 +52,37 @@ Search.grid(row=2, column=0, padx = 0, pady = 0)
 
 #text for coin name
 Coin_Name = tk.Label(root,text = "",font=("bold", 18),fg ="green")
-Coin_Name.grid(row=0,column=90,padx = 100)
+Coin_Name.grid(row=3,column=0,pady=(20,0))
 
 #text for price
 price = tk.Label(root,text = "",font=("bold", 12),fg ="green")
-price.grid(row=1,column=90,padx = 100)
+price.grid(row=4,column=0)
+
+# #price you want to get notified
+l2 = tk.Label(root,text = "Enter Price",font=("bold", 18))
+l2.grid(row=100,pady=(20,0))
+
+price_text = tk.StringVar()
+price_entry = tk.Entry(root, textvariable=price_text)
+price_entry.grid(row=110, column=0, padx=0, pady=0)
+
+Set_Notifier = tk.Button(root, text="Set Notifier",command=notify)
+Set_Notifier.grid(row=120, column=0,pady=0)
+
 
 #date selection for price history
-# l2 = tk.Label(root,text = "Select date for price and other history",font=("bold", 14))
-# l2.grid(row=100,column=0)
+l3 = tk.Label(root,text = "Select date for price and other history",font=("bold", 14))
+l3.grid(row=0,column=100,padx=(300,0))
 # mycal1 = Calendar(root,setmode="day", date_pattern = 'd/m/yy')
-# mycal1.place(x=220, y=200)
-# Generate_Data = tk.Button(root, text="Generate Data",command=generate_data)
-# Generate_Data.grid(row=4000, column=0)
+# mycal1.place(x=220, y=200,padx=(100,0))
+Generate_Data = tk.Button(root, text="Generate Data",command=generate_data)
+Generate_Data.grid(row=1, column=100,padx=(300,0))
 
 
 # #price you want to get notified
 # l3 = tk.Label(root,text = "Price to get notified",font=("bold", 14))
 # l3.grid(row=0,column=70)
 
-l2 = tk.Label(root,text = "Enter Price",font=("bold", 18))
-l2.grid(row=60,column=0)
-
-price_text = tk.StringVar()
-price_entry = tk.Entry(root, textvariable=price_text)
-price_entry.grid(row=70, column=0, padx=0, pady=0)
-
-Set_Notifier = tk.Button(root, text="Set Notifier",command=notify)
-Set_Notifier.grid(row=80, column=0)
 #update the price every 5 seconds
 def rep():
     search_coin_clicked()
